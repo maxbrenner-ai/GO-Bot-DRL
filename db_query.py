ee8f664d60faa09fcd07e608ba43ed78d5c4b0c9
maxbren <maxbrenner110@gmail.com>
2018-09-22 01:34:52 -0400

    Update

--- db_query.py
+++ db_query.py
@@ -4,19 +4,66 @@ from collections import defaultdict
 class DBQuery:
     def __init__(self, database):
         self.database = database
+        # {frozenset: {string: int}} A dict of dicts
         self.cached_db_slot = defaultdict(dict)
+        # {frozenset: {'#': {'slot': 'value'}}} A dict of dicts of dicts, a dict of DB sub-dicts
+        self.cached_db = defaultdict(dict)
 
     def fill_inform_slots(self, inform_slots_to_fill, current_inform_slots):
+        # db_results is a dict of dict in the same exact format as the db, its just a subset of the db
+        db_results = self._get_db_results(current_inform_slots)
         return inform_slots_to_fill
+
+    def _get_db_results(self, inform_slots):
+        constrain_keys = inform_slots.keys()
+        # Todo: get rid of this filter in mine
+        constrain_keys = filter(lambda k: k != 'numberofpeople', constrain_keys)
+        # Cuz we dont want to constrain results for a slot that the usersim doesnt care about
+        constrain_keys = [k for k in constrain_keys if inform_slots[k] != 'anything']
+
+        inform_items = frozenset(inform_slots.items())
+        cache_return = self.cached_db[inform_items]
+
+        if cache_return is None:
+            # If it is none then no matches fit with the constraints so return an empty dict
+            return dict()
+        # if it isnt empty then return what it is
+        if cache_return:
+            return cache_return
+        # else continue on
+
+        available_movies = {}
+        for id in self.database.keys():
+            current_movie_dict = self.database[id]
+            # First check if that databse movie actually contains the inform keys
+            # Todo: again this assumes that if a slot key is not in a movie dict then it doesnt match
+            if len(set(constrain_keys) - set(self.database[id].keys())) == 0:
+                match = True
+                # Now check all the slots values against the DB movie slots values and if there is a mismatch dont store
+                for k, v in inform_slots.items():
+                    # Todo: get rid of lower
+                    if str(v).lower() != str(current_movie_dict[k]).lower():
+                        match = False
+                if match:
+                    # Update cache
+                    self.cached_db[inform_items].update(current_movie_dict)
+                    available_movies.update(current_movie_dict)
+
+        # if nothing avail then set it to none in cache
+        if not available_movies:
+            self.cached_db[inform_items] = None
+
+        return available_movies
 
     def results_for_slots(self, current_informs):
         # The items (key, value) of the current informs are used as a key to the cached_db_slot
         inform_items = frozenset(current_informs.items())
         # A dict of the inform keys and their counts as stored (or not stored) in the cached_db_slot
-        slot_count_dict = self.cached_db_slot[inform_items]
+        # Todo: Do time tests on multiple different DB to see if caching queries actually makes training faster overall
+        cache_return = self.cached_db_slot[inform_items]
         # This is the same as checking if its not empty, ie the current informs have been used to query before
-        if slot_count_dict:
-            return slot_count_dict
+        if cache_return:
+            return cache_return
 
         # If it made it down here then a new query was made and it must add it to cached_db_slot and return it
         # init all key values with 0
