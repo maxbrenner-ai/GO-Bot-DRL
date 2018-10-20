767179749032bd24aee283a68a349d7b85fa84ea
maxbrenner <maxbrenner110@gmail.com>
2018-10-20 11:16:39 -0400

    Final before NL

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 '''
 
-- Add temp space for NL
+- Add temp space for NL (but dont actually keep)
 - Setup way to chat to bot (in frames)
-
-
-- NO TIME FOR ANYTHING ELSE
+    - With saving and loading
+- Do a hyp param search with LR (if i have the time)
+- Gather final data
+    - In even more detail, and longer trains
+- Clean code
+    - Rename anything that needs it
+- Fix/Add/Remove Comments
+- Add method descriptions
+- Possibly add argument parser for using the console
 
 '''
