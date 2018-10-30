ebcb3f831c51cb6eb41d5a10296082548f09cf6a
maxbrenner <maxbrenner110@gmail.com>
2018-10-29 20:37:19 -0400

    Final before saving/loading

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 '''
 
-- Add temp space for NL (but dont actually keep)
 - Setup way to chat to bot (in frames)
     - With saving and loading
-- Do a hyp param search with LR (if i have the time)
 - Gather final data
     - In even more detail, and longer trains
 - Clean code
