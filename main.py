dc07fc41ab62d85b3e31dddf95930e571c30fe89
maxbren <maxbrenner110@gmail.com>
2018-11-01 13:43:44 -0400

    Final Before Re-structure usersim

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 '''
 
-- DEBUG USER
-- Gather final data
-    - In even more detail, and longer trains
 - Clean code
     - Rename anything that needs it
 - Fix/Add/Remove Comments
 - Add method descriptions
 - Possibly add argument parser for using the console
+- Gather final data
+    - In even more detail, and longer trains
 
 
 Notes:
